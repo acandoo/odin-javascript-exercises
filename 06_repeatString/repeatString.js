232174037636134e2467d635f9faf24f86aaa82f
acandoo <117209328+acandoo@users.noreply.github.com>
2025-06-10 23:17:21 -0400

    06 solved

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num > 0) {
+        return repeatString(string, num - 1) + string;
+    }
+    if (num < 0) {
+        return "ERROR";
+    }
+    return "";
 };
 
 // Do not edit below this line

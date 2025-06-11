829c14ec5e34641b5e85f46a3545bcff00566b41
acandoo <117209328+acandoo@users.noreply.github.com>
2025-06-11 01:17:27 -0400

    09 solved

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(x, y) {
+    if (!(typeof x === "number" && typeof y === "number")) {
+        return "ERROR";
+    }
+    if (!(Number.isInteger(x) && Number.isInteger(y))) {
+        return "ERROR";
+    }
+    if (x + y !== Math.abs(x + y)) {
+        return "ERROR";
+    }
+    const biggerNum = (x > y) ? x : y;
+    const smallerNum = (x < y) ? x : y;
+    return [...Array(biggerNum + 1).keys()]
+        .splice(smallerNum)
+        .reduce((partialSum, a) => partialSum + a);
 };
 
 // Do not edit below this line

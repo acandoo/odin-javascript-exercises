fe1948342dbc7f3a44e7c8229a4bb5e82351da9b
acandoo <117209328+acandoo@users.noreply.github.com>
2025-06-10 23:21:10 -0400

    07 solved

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let newString = '';
+    for (const char of string) {
+        newString = char + newString;
+    }
+    return newString
 };
 
 // Do not edit below this line

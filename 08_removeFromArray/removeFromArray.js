e301ad86a5968f91c5bb81d7e20e942b4fbb9537
acandoo <117209328+acandoo@users.noreply.github.com>
2025-06-11 01:01:35 -0400

    08 solved

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function(...args) {
+    let newArr = [];
+    for (const i of args[0]) {
+        if (!args.includes(i, 1)) newArr.push(i);
+    }
+    return newArr;
 };
 
 // Do not edit below this line

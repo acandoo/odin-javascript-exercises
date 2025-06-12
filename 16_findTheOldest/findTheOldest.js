882145681fee5ae5a2e23a88b44e5a9d211d4da2
acandoo <117209328+acandoo@users.noreply.github.com>
2025-06-12 00:33:46 -0400

    16 solved

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let ages = people.map(person => ((person.yearOfDeath) ? (person.yearOfDeath) : new Date().getFullYear()) - person.yearOfBirth);
+    let oldestPersonNum = 0;
+    for (const i of Array(ages.length).keys()) {
+        if (ages[i] > ages[oldestPersonNum]) {
+            oldestPersonNum = i;
+        }
+    }
+    return people[oldestPersonNum];
+    // return ages.reduce((prev, current) => (prev > current) ? prev : current);
 };
 
 // Do not edit below this line

9a7bf4d7fb62f5bbc2fcae5e517bc6a89da9e7c9
acandoo <117209328+acandoo@users.noreply.github.com>
2025-06-11 22:32:57 -0400

    12 solved

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,17 @@
-const add = function() {
-	
-};
+const add = (a, b) => a + b;
 
-const subtract = function() {
-	
-};
+const subtract = (a, b) => a - b;
 
-const sum = function() {
-	
-};
+const sum = (arr) => (arr.length !== 0) ? arr.reduce((prev, current) => prev + current) : 0;
 
-const multiply = function() {
+const multiply = (arr) => arr.reduce((prev, current) => prev * current);
 
-};
+const power = (a, n) => a ** n;
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num === 0) return 1;
+  if (num < 0) return "ERROR";
+  return parseInt(num) * factorial(parseInt(num) - 1);
 };
 
 // Do not edit below this line

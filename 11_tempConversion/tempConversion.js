48891402f47bcd5971589413c5f3c01e8d854798
acandoo <117209328+acandoo@users.noreply.github.com>
2025-06-11 21:57:49 -0400

    11 solved

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(f) {
+  const unrounded = (f - 32) * 5 / 9;
+  return Math.round(unrounded * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(f) {
+  const unrounded = f * 9 / 5 + 32;
+  return Math.round(unrounded * 10) / 10;
 };
 
 // Do not edit below this line

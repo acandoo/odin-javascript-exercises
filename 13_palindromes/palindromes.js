2a56842980c6ca8304fb51d27c9ec4af7d302d20
acandoo <117209328+acandoo@users.noreply.github.com>
2025-06-11 22:44:37 -0400

    13 solved

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    if (typeof str !== "string") return "ERROR";
 
+    // remove characters
+    let newStr = [];
+    const filteredChars = [",", ".", "!", " ", ";", ":", "-"];
+    for (const i of str) {
+        if (!filteredChars.includes(i)) newStr.push(i.toLowerCase());
+    }
+
+    // not efficient but who cares this is javascript
+    for (const i of Array(newStr.length).keys()) {
+        if (newStr[i] !== newStr[newStr.length - 1 - i]) return false;
+    }
+    return true;
 };
 
 // Do not edit below this line
